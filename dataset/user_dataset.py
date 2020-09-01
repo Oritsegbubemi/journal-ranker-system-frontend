f8a200ff9a44e31251a14b3a4b06b2572d73e6b7
Gbubemi Makpokpomi <gbubemimakpokpomi@gmail.com>
2020-09-01 11:43:24 +0100

    made fixes

--- dataset/user_dataset.py
+++ dataset/user_dataset.py
@@ -54,6 +54,4 @@ def user_psi_dataset():
     # 10 - Exporting to Result Ranking dataset
     ranked_journal.to_csv('dataset/result_dataset.csv', index=False)
     ranked_journal.to_html('static/user_table.html')
-    print("File has been created")
-    print("Ranking Table has been created")
     

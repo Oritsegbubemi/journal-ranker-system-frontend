03466994073a3f297dfdef056cd4b67a40e105d0
Oritsegbubemi <gbubemimakpokpomi@gmail.com>
2020-11-09 07:03:50 +0100

    added netlify link

--- journals/settings.py
+++ journals/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_JOURNAL')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'journal-ranker.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'journal-ranker.herokuapp.com', 'journalranker.netlify.app']
 
 
 # Application definition

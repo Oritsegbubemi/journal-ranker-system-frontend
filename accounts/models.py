909d2fd769d24cd5b7df31838c7c57b3644b057f
Oritsegbubemi <gbubemimakpokpomi@gmail.com>
2020-11-10 03:06:37 +0100

    added a default username

--- accounts/models.py
+++ accounts/models.py
@@ -59,7 +59,7 @@ class User(AbstractUser):
         ('Stu', 'Student'),
         ('Others', 'Others'),
     )
-    username = models.CharField(max_length=100, blank=False)
+    username = models.CharField(max_length=100, blank=False, default="journal")
     email = models.EmailField(max_length=250, unique=True)
     institution = models.CharField(choices=INSTITUTION, max_length=20, blank=True, null=True)
     level = models.CharField(choices=LEVEL, max_length=10, blank=True, null=True)

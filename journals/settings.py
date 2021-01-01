d59ebf76586d1fa44f90020fd7566f4c983ab2ea
Oritsegbubemi <gbubemimakpokpomi@gmail.com>
2021-01-01 21:52:34 +0100

    ready for heroku

--- journals/settings.py
+++ journals/settings.py
@@ -1,5 +1,6 @@
 import os
 import django_heroku
+import dj_database_url
 from dotenv import load_dotenv
 
 
@@ -10,12 +11,13 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'journal-ranker.herokuapp.com', 'journalranker.netlify.app']
 
 
@@ -84,16 +86,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
-# Added 
-del DATABASES['default']['OPTIONS']['sslmode']
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -132,14 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 LOGIN_URL = 'login'
-
 AUTH_USER_MODEL = 'accounts.User' 
 
 # Send Email via GSuite
@@ -147,5 +137,15 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = 'PassionateJ1!'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
+
+# db_from_env
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+# Added 
+del DATABASES['default']['OPTIONS']['sslmode']

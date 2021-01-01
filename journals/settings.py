10ea067e64ec75927705bd8ae0708e7e07cd49fd
Oritsegbubemi <gbubemimakpokpomi@gmail.com>
2021-01-02 00:11:07 +0100

    changed to postgres database

--- journals/settings.py
+++ journals/settings.py
@@ -68,23 +68,23 @@ WSGI_APPLICATION = 'journals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ['DB_JOURNAL_NAME'],
-#         'USER': os.environ['DB_USER'],
-#         'PASSWORD': os.environ['DB_PASSWORD'],
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'de7t719dbptl8i',
+        'HOST': 'ec2-3-208-50-226.compute-1.amazonaws.com',
+        'USER': 'mnsfaqmushcxvt',
+        'PASSWORD': '93870378dadf14ed34cc332a88b6090af341409e4da6bd8d748ca32ba9d5d932',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation

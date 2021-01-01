3e7ce429230c8a49a2492b3cdf9b7c0af4e66900
Oritsegbubemi <gbubemimakpokpomi@gmail.com>
2021-01-01 23:20:22 +0100

    added staticfiles

--- journals/settings.py
+++ journals/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'login'
 AUTH_USER_MODEL = 'accounts.User' 
